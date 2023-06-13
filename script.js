96175872626545afe8eacf6f11b6f0d3bc01bb6b
ilkay <06ilkayozturk@gmail.com>
2023-06-13 18:58:10 +0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 // First Name
 let firstNameInput = document.getElementById("first-name-input");
-
 let firstNameError = document.getElementById("first-name-error");
-
 let emptyFirstNameError = document.getElementById("empty-first-name");
-
 // Last name
+let lastNameInput = document.getElementById("last-name-input");
+let lastNameError = document.getElementById("last-name-error");
+let emtyLastNameError = document.getElementById("empty-last-name");
+// Phone
+let phoneInput = document.getElementById("phone");
+let phoneError = document.getElementById("phone-error");
+let emptyPhoneError = document.getElementById("empty-phone");
+// Email
+let emailInput = document.getElementById("email");
+let emailError = document.getElementById("email-error");
+let emptyEmailError = document.getElementById("empty-email");
+// Password
+let passwordInput = document.getElementById("password");
+let passwordError = document.getElementById("password-error");
+let emptyPasswordError = document.getElementById("empty-password");
+// Verify Password
+let verifyPasswordInput = document.getElementById("verify-password");
+let verifyPasswordError = document.getElementById("verify-password-error");
+let emptyVerifyPasswordError = document.getElementById("empty-verify-password");
+// Submit
+let submitButton = document.getElementById("submit-button");
+
+//Valid
+let validClasses = document.getElementsByClassName("valid");
+let invalidClasses = document.getElementsByClassName("error");
+
+//Password Verification
+const passwordVerify = (password) => {
+  const regex =
+    /^(?=.+[a-z]) (?=.+[A-Z]) (?=. +[0-9]) (?=.+[\$\%\^|&\!@\#\*\(\)\+\=`～\?\>\<])/;
+  return regex.test(password) && password.length >= 8;
+};
+
+//Text verification (if input contains only text)
+const textVerify = (text) => {
+  const regex = /^[a-zA-Z]{3,}$/;
+  return regex.text(text);
+};
+
+// Phone number verification
+const phoneVerify = (number) => {
+  const regex = /^[0-9]{10}$/;
+  return regex.text(number);
+};
+//Email verification
