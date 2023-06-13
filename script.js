8279f24d6114c96c09085a62381039fa0065b276
ilkay <06ilkayozturk@gmail.com>
2023-06-13 13:21:21 +0300

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// First Name
+let firstNameInput = document.getElementById("first-name-input");
+
+let firstNameError = document.getElementById("first-name-error");
+
+let emptyFirstNameError = document.getElementById("empty-first-name");
+
+// Last name
